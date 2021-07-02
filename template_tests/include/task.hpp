15141b03b0c2687ee5c5a499118b2ca81c641d67
guillaume arreckx <software@arreckx.com>
2021-07-02 21:29:53 +0200

    New C++ task class working

--- template_tests/include/task.hpp
+++ template_tests/include/task.hpp
@@ -14,6 +14,7 @@
 
 #include <etl/delegate.h>
 
+
 enum class priority_t : UBaseType_t
 {
    idle = tskIDLE_PRIORITY,
@@ -21,45 +22,21 @@ enum class priority_t : UBaseType_t
    high
 };
 
-namespace taskname
-{
-   template <char... Chrs>
-   struct string<char, Chrs...> {
-      using type = string;
-      static auto c_str() {
-         static constexpr char str[] = {Chrs..., 0};
-         return str;
-      }
-   };
-
-   template <class T>
-   struct string<T> {
-      using type = T;
-      static auto c_str() { return c_str_impl((T *)0); }
-      template <class U>
-      static decltype(U::c_str()) c_str_impl(U *) {
-         return U::c_str();
-      }
-      static auto c_str_impl(...) { return get_type_name<T>(); }
-   };
-}
-
-template <class T, T... Chrs>
-constexpr auto operator""_taskname() {
-   return string<T, Chrs...>{};
-}
+   /** Trampoline entry point for C */
+   static extern "C" entrypoint(Task *arg)
+   {
+      EntryPoint(arg);
+   }
 
 
 template<
    typename TName,
    const size_t TStackSize=255,
    const priority_t TPriority=priority_t::normal,
-   typename TArg = void
 > 
 class Task
 {
    constexpr stack_size = configMINIMAL_STACK_SIZE + TStackSize;
-   using RunCb_t = etl::delegate<void(typename TArg *)>;
    
    /** Structure that will hold the TCB of the task being created. */
    StaticTask_t taskBuffer;
@@ -69,31 +46,23 @@ class Task
 
    /** Handle to the task */   
    TaskHandle_t handle_;
-   
-   /** Store the parameter arg */
-   TArg *arg_ptr_;
-      
-   /** Trampoline entry point for C */
-   static extern "C" entrypoint(Task *arg)
-   {
-      EntryPoint(arg);
-   }
-   
-
+  
 public:
    TaskHandle_t operator *()
    {
       return handle_;
    }
    
-   template< class Function, class... Args >
-   explicit Task( Function&& f, Args&&... args );   
+   static constexpr const char *get_name() const
+   { return TName::name(); }
+  
    
    Task() : handle_(nullptr)
    {
    }
    
-   void start(TArg *arg) 
+   template< class Function, class... Args >
+   void start( Function&& f, Args&&... args )
    {
         /* Create the task without using any dynamic memory allocation. */
         handle_ = xTaskCreateStatic(
