bf4418e50e9d951372f84dafaf0cebc926200121
guillaume arreckx <software@arreckx.com>
2021-08-27 22:23:00 +0200

    Simulator getting better

---
+++ template_tests/src/sequencer_worker.cpp
@@ -0,0 +1,118 @@
+/*
+ * Sequencer router
+ *
+ * Created: 29/06/2021 21:26:21
+ * Author : software@arreckx.com
+ */
+
+#include "sequencer_worker.hpp"
+
+#include <logger.h>
+
+
+using namespace rtos::tick;
+
+namespace
+{
+   const char *const DOM = "sq.worker";
+}
+
+
+SequencerWorker::SequencerWorker( CycloManager &pgm_man )
+   : timer_counter{ 100 }, ticks_left{ 0 }, pgm_man{ pgm_man }
+{}
+
+// Activate the sequencer. This resets the program
+void SequencerWorker::on_receive( const msg::StartProgram &msg )
+{
+   LOG_TRACE( DOM, "StartProgram" );
+
+   if ( msg.from_start )
+   {
+      // Reset the counter
+      pgm_man.set_counter( 0 );
+
+      // Update the GUI
+      fx::publish( msg::CounterUpdate{} );
+
+      // Start from the start
+      pgm_man.get_command().start();
+
+      ticks_left = 0;
+   }
+   else if ( ticks_left )
+   {
+      timer.set_param( timer_counter );
+      timer.set_period( ticks_left );
+      timer.start();
+
+      return;
+   }
+
+   execute_next();
+}
+
+void SequencerWorker::on_receive( const msg::StopProgram &msg )
+{
+   LOG_TRACE( DOM, "StopProgram" );
+
+   ticks_left = timer.get_time_left_until_expiry();
+
+   // Invalidate the current timer instance to prevent a race (the timer message is already in
+   // the queue)
+   ++timer_counter;
+
+   // Stop the timer
+   timer.stop();
+}
+
+void SequencerWorker::on_receive( const msg::SequenceNext &msg )
+{
+   LOG_TRACE( DOM, "SequenceNext" );
+
+   // Check the timer is still valid (avoid race)
+   if ( timer.get_param() == timer_counter )
+   {
+      execute_next();
+   }
+}
+
+void SequencerWorker::execute_next()
+{
+   LOG_HEADER( DOM );
+
+   // Pick the first command and apply it
+   auto &command = pgm_man.get_command();
+
+   // Grab the first item and move the iterator
+   const CommandItem *item = command.iterate();
+
+   // Make sure not the last
+   if ( item != nullptr )
+   {
+      // Execute the item
+      switch ( item->command )
+      {
+      case CommandItem::close: pgm_man.get_contact().set( Contact::close ); break;
+      case CommandItem::open: pgm_man.get_contact().set( Contact::open ); break;
+      case CommandItem::delay:
+         // Do nothing
+         break;
+      case CommandItem::loop:
+         // Start all over
+         pgm_man.get_command().start();
+
+         pgm_man.set_counter( pgm_man.get_counter() + 1 );
+         fx::publish( msg::CounterUpdate{} );
+
+         // Avoid recursion - save the stack, save the trouble - post again
+         fx::publish( msg::SequenceNext{} );
+         return;
+      }
+
+      // Fire a new timers
+      timer.set_param( ++timer_counter );
+      timer.set_period( rtos::tick::from_ms( item->delay_ms ) );
+      timer.start();
+   }
+}
